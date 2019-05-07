f960baf8370849fd66967ba826461ee801745e69
AndréSDS <andre_smiths@outlook.com>
2019-05-07 00:03:04 -0300

    alterando import do CursoService no cursos.component

--- loiane/rotas/src/app/cursos/curso.service.ts
+++ loiane/rotas/src/app/cursos/curso.service.ts
@@ -5,18 +5,19 @@ import { Injectable } from '@angular/core';
 })
 export class CursoService {
 
-  getCursos(){
+  getCursos() {
     return [
     {id: 1, nome: 'angular 2'},
     {id: 2, nome: 'Java'}
    ];
   }
 
-  getCurso(id: number){
+  getCurso(id: number) {
     const cursos = this.getCursos();
-    for (let i = 0; i < cursos.length; i++){
-      let curso = cursos[i];
-      if (curso.id == id){
+// tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < cursos.length; i++) {
+      const curso = cursos[i];
+      if (curso.id === id) {
         return curso;
       }
     }
--- loiane/rotas/src/app/cursos/cursos.component.ts
+++ loiane/rotas/src/app/cursos/cursos.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { CursoService } from './../../../../servicos/src/app/cursos/curso.service';
-import { ActivatedRoute } from '@angular/router';
+import { CursoService } from './curso.service';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -16,11 +16,13 @@ export class CursosComponent implements OnInit {
 
   constructor(
     private cursoService: CursoService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router: Router
     ) { }
 
   ngOnInit() {
     this.cursos = this.cursoService.getCursos();
+
     this.inscricao = this.route.queryParams.subscribe(
       (queryParams: any) => {
         this.pagina = queryParams.pagina;
@@ -32,6 +34,14 @@ export class CursosComponent implements OnInit {
 // tslint:disable-next-line: use-life-cycle-interface
   ngOnDestroy() {
     this.inscricao.unsubscribe();
+  }
+
+  proximaPagina() {
+    // this.pagina++;
+    this.router.navigate(
+      ['/cursos'],
+      {queryParams: {pagina: ++this.pagina}}
+      );
   }
 
 }
