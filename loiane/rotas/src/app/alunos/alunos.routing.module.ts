31f0596abe2cb83f27f22c267768b360c215053c
AndréSDS <andre_smiths@outlook.com>
2019-05-27 22:59:37 -0300

    aula 60 - criando alunos.component e rotas filhas

---
+++ loiane/rotas/src/app/alunos/alunos.routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AlunosComponent } from './alunos.component';
+import { AlunoDetalheComponent } from './aluno-detalhe/aluno-detalhe.component';
+import { AlunoFormComponent } from './aluno-form/aluno-form.component'
+
+const alunosRoutes = [
+  {path: 'alunos', component: AlunosComponent, children: [
+    {path: 'novo', component: AlunoFormComponent},
+    {path: ':id', component: AlunoDetalheComponent},
+    {path: ':id/editar', component: AlunoFormComponent}
+  ]},
+];
+
+@NgModule ({
+  imports: [RouterModule.forChild(alunosRoutes)],
+  exports: [RouterModule]
+})
+
+export class AlunosRoutingModule {}
