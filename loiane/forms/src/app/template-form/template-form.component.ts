8184d5db27b0597b5dfb118eb76fb9ec8eb99909
AndréSDS <andre_smiths@outlook.com>
2019-09-01 12:32:49 -0300

    iniciando com formulários

---
+++ loiane/forms/src/app/template-form/template-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-template-form',
+  templateUrl: './template-form.component.html',
+  styleUrls: ['./template-form.component.css']
+})
+export class TemplateFormComponent implements OnInit {
+
+  usuario: any = {
+    nome: 'André',
+    email: 'andrsSDS@outlook.com'
+  };
+
+  onSubmit(form) {
+    console.log(form);
+
+    console.log(this.usuario);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
