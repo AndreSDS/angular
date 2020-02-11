940984873a9a4382c638df41814702d3a492de2d
André Souza <andre_smiths@outlook.com>
2020-02-11 00:01:32 -0300

    aula 114 - craindo campo input reutilizavel na pasta shared

--- loiane/forms/src/app/shared/shared.module.ts
+++ loiane/forms/src/app/shared/shared.module.ts
@@ -3,23 +3,28 @@ import { CampoControlComponent } from './campo-control-erro/campo-control.compon
 import { FormDebugComponent } from './form-debug/form-debug.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { DropdownService } from './services/dropdown.service';
 import { ErrorMsgComponent } from './error-msg/error-msg.component';
+import { InputFieldComponent } from './input-field/input-field.component';
 
 @NgModule({
   declarations: [
     FormDebugComponent,
     CampoControlComponent,
-    ErrorMsgComponent
+    ErrorMsgComponent,
+    InputFieldComponent
   ],
   exports: [
     FormDebugComponent,
     CampoControlComponent,
-    ErrorMsgComponent
+    ErrorMsgComponent,
+    InputFieldComponent
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [ DropdownService ]
 })
