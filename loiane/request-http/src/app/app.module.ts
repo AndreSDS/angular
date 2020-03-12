27031713869c536f0f7686a92ee2875d6e6787b8
andresds <andre_smiths@outlook.com>
2020-03-12 14:54:45 -0300

    criando service para requisição GET e mostrando dados na tela com *ngFor

--- loiane/request-http/src/app/app.module.ts
+++ loiane/request-http/src/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,9 +11,8 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    BsDropdownModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
