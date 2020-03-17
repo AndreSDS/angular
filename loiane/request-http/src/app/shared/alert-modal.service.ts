fd43a8192f67432e72a0bda0acadc76434987b2d
andresds <andre_smiths@outlook.com>
2020-03-17 12:05:03 -0300

    form para criar novo curso

---
+++ loiane/request-http/src/app/shared/alert-modal.service.ts
@@ -0,0 +1,31 @@
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { Injectable } from '@angular/core';
+import { AlertModalComponent } from './alert-modal/alert-modal.component';
+
+enum AlertTypes {
+  DANGER = 'danger',
+  SUCCESS = 'success'
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertModalService {
+
+  constructor(private modalService: BsModalService) { }
+
+  private showAlert(message: string, type: string){
+    const bsModalRef: BsModalRef = this.modalService.show(AlertModalComponent);
+    bsModalRef.content.type = type;
+    bsModalRef.content.message = message;
+  }
+
+  showAlertDanger(message: string){
+    this.showAlert(message, AlertTypes.DANGER);
+  }
+
+  showAlertSuccess(message: string){
+    this.showAlert(message, AlertTypes.SUCCESS);
+  }
+
+}
