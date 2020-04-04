2f04b6e2a4b4563dd5a555e207836c0ef155d5c7
andresds <andre_smiths@outlook.com>
2020-04-04 01:33:02 -0300

    aplicando algum css

--- ng-blog/src/app/app-routing.module.ts
+++ ng-blog/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
+import { AboutComponent } from './about/about.component';
+import { ArticleListComponent } from './article-list/article-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'articles', component: ArticleListComponent },
+  { path: 'about', component: AboutComponent },
+  { path: '', component: ArticleListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
