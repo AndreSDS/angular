3997b3a5d0c8ade0c6a682f159b63a22003b1598
andresds <andre_smiths@outlook.com>
2020-04-15 22:38:23 -0300

    teste

--- ng-blog/src/app/article.ts
+++
@@ -1,9 +0,0 @@
-export class Article{
-    id: number;
-    title: string = '';
-    content: string = '';
-    key:string = '';
-    date: Date = new Date();
-    description:string = '';
-    imageUrl:string = '';
-}
